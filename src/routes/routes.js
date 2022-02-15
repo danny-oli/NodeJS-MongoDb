bf4d3a07abd836f94ee6f72780954cbca893f297
Danny Oliveira <54421047+danny-oli@users.noreply.github.com>
2022-02-15 19:38:56 -0300

    Update routes/routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,23 +1,15 @@
 const express = require("express");
+const router = express.Router();
 
-module.exports = (app) => {    
-    const router = express.Router()
+const userRoutes = require("../model/User/routes")(router);
+const authRoutes = require("../model/Auth/routes")(router);
+const substanceRoutes = require("../model/Substance/routes")(router);
+const toxinTestRoutes = require("../model/ToxinTest/routes")(router);
 
-    const userRoutes = require("../model/User/routes")(router);
-    const authRoutes = require("../model/Auth/routes")(router);
-    const substanceRoutes = require("../model/Substance/routes")(router);
-    const toxinTestRoutes = require("../model/ToxinTest/routes")(router);
-
-    app.use("/api/user", userRoutes);
-    app.use("/api/auth", authRoutes);
-    app.use("/api/substance", substanceRoutes);
-    app.use("/api/toxinTest", toxinTestRoutes);
-    
-    return app;
+module.exports = (app) => {
+  app.use("/api/user", userRoutes);
+  app.use("/api/auth", authRoutes);
+  app.use("/api/substance", substanceRoutes);
+  app.use("/api/toxinTest", toxinTestRoutes);
+  return app;
 };
-
-
-
-
-
-
